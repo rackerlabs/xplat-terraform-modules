839b517dbce18c39d2bdb4692ae4fb1904e3afe2
Bruce Stringer <bruce.stringer@rackspace.com>
2020-03-19 12:30:41 -0500

    Add lambda-with-s3

---
+++ modules/lambda-with-s3/variables.tf
@@ -0,0 +1,95 @@
+variable "alarm_actions" {
+  type    = list(string)
+  default = []
+}
+
+variable "ok_actions" {
+  type    = list(string)
+  default = []
+}
+
+variable "enable_monitoring" {
+  type    = string
+  default = 0
+}
+
+variable "env_variables" {
+  type    = map(string)
+  default = {}
+}
+
+variable "source_code_hash" {
+}
+
+variable "s3_bucket" {
+}
+
+variable "s3_key" {
+}
+
+variable "handler" {
+  type    = string
+  default = "handler.lambda_handler"
+}
+
+variable "memory_size" {
+  type    = string
+  default = 256
+}
+
+variable "name" {
+}
+
+variable "publish" {
+  type    = string
+  default = true
+}
+
+variable "stage" {
+}
+
+variable "runtime" {
+  type    = string
+  default = "python3.6"
+}
+
+variable "timeout" {
+  type    = string
+  default = 60
+}
+
+variable "description" {
+  type    = string
+  default = ""
+}
+
+variable "kms_key_arn" {
+  type    = string
+  default = ""
+}
+
+variable "tracing_mode" {
+  type    = string
+  default = "PassThrough"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "reserved_concurrent_executions" {
+  type    = string
+  default = "-1"
+}
+
+variable "throttle_threshold" {
+  type    = string
+  default = "1"
+}
+
+variable "layers" {
+  type    = list(string)
+  default = []
+}
+
