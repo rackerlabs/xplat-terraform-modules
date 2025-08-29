2cfd12de20b8824b003ce2cd2f781768e9cb43cb
Manivannan <manivannan.adimoolam@rackspace.com>
2025-08-29 14:44:33 +0530

    removed stage dependency

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -107,21 +107,14 @@ resource "aws_api_gateway_domain_name" "domain" {
   tags = var.tags
 }
 
-resource "aws_api_gateway_stage" "stage" {
-  stage_name    = var.stage
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  deployment_id = aws_api_gateway_deployment.stage.id
-}
-
 resource "aws_api_gateway_base_path_mapping" "basepath" {
   count = var.enable_custom_domain ? 1 : 0
 
   api_id      = aws_api_gateway_rest_api.api.id
-  stage_name  = aws_api_gateway_stage.stage.stage_name
+  stage_name  = var.stage
   domain_name = aws_api_gateway_domain_name.domain[0].domain_name
   base_path   = var.base_path
 
-  depends_on = [aws_api_gateway_stage.stage]
 }
 
 data "aws_route53_zone" "domain" {
